d0338a010bddf3353b94de998a5b33ce2e95b164
Viktoria <viktoriyadem123456789@gmail.com>
2024-10-20 00:02:47 +0300

    20.10

---
+++ src/components/ButtonComponent.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+// import { ref } from "vue";
+
+const props = defineProps<{
+  label: string;
+  disabled: boolean;
+  onClick: () => void;
+}>();
+</script>
+
+<template>
+  <button
+    @click.prevent="props.onClick"
+    :disabled="props.disabled"
+    class="btn btn-info"
+  >
+    {{ props.label }}
+  </button>
+</template>
+
+<style lang="scss">
+@import "./scss/styles.scss";
+</style>
---
+++ src/components/InputField.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+const props = defineProps<{
+  modelValue: string;
+  label: string;
+  type: string;
+  error: boolean;
+  errorMessage: string;
+  placeholder: string;
+}>();
+
+const emit = defineEmits<{
+  (e: "update:modelValue", value: string): void;
+}>();
+
+function updateValue(event: Event) {
+  const target = event.target as HTMLInputElement;
+  emit("update:modelValue", target.value);
+}
+</script>
+
+<template>
+  <div class="mb-3">
+    <label class="form-label fs-5 fw-bold">{{ props.label }}</label>
+    <input
+      :value="props.modelValue"
+      @input="updateValue"
+      :type="props.type"
+      class="form-control"
+      :placeholder="props.placeholder"
+      :class="{
+        'is-invalid': props.error,
+        'is-valid': !props.error,
+      }"
+    />
+    <span v-if="props.error" class="text-danger">{{ props.errorMessage }}</span>
+  </div>
+</template>
