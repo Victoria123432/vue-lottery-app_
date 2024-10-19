139b764cdca999fb66df19a714915425e241c9df
Viktoria <viktoriyadem123456789@gmail.com>
2024-10-20 00:03:28 +0300

    20.10

---
+++ src/components/WinnerComponent.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const props = defineProps<{
+  winners: [];
+}>();
+</script>
+
+<template>
+  <span
+    v-for="(winner, index) in props.winners"
+    :key="index"
+    class="p-2 bg-info text-white rounded me-2"
+  >
+    {{ winner.name }}
+    <button
+      class="btn btn-info btn-sm ms-2"
+      @click.prevent="deleteWinner(index)"
+    >
+      x
+    </button>
+  </span>
+</template>
+
+<style lang="scss">
+@import "./scss/styles.scss";
+</style>
